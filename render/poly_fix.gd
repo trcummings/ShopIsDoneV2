d4e8698fc469955ed8590ea398e526c82cdd4b3f
Thomsen Cummings <thomsencummings@gmail.com>
2024-01-13 18:25:36 -0500

    update poly fix to work different in game vs in the editor

--- render/poly_fix.gd
+++ render/poly_fix.gd
@@ -3,5 +3,8 @@ extends Polygon2D
 
 func _ready():
 	var use_custom_rect = true;
-	var rect = get_viewport_rect();
+	var rect = Rect2(0, 0, 2000, 2000)
+	# if we're in the editor, just use the viewport rect
+	if Engine.is_editor_hint():
+		rect = get_viewport_rect();
 	RenderingServer.canvas_item_set_custom_rect(get_canvas_item(), use_custom_rect, rect)
