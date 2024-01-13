8f218702dbfaff2411ee6cfd6392a33860b229d8
Thomsen Cummings <thomsencummings@gmail.com>
2024-01-13 12:36:04 -0500

    add in missing extensions for progress bar widget

---
+++ util/extensions/ControlExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Godot;
+
+namespace ShopIsDone.Utils.Extensions
+{
+	public static class ControlExtensions
+	{
+        public static async Task FadeIn(
+            this Control control,
+            float duration,
+            Tween.EaseType ease = Tween.EaseType.Out,
+            Tween.TransitionType trans = Tween.TransitionType.Linear
+        )
+        {
+            control.Modulate = Colors.Transparent;
+            control.Show();
+
+            // Tween in icon
+            var tween = control
+                .GetTree()
+                .CreateTween()
+                .TweenProperty(control, "modulate:a", 1f, duration)
+                // Set ease and trans type
+                .SetEase(ease)
+                .SetTrans(trans);
+
+            await control.ToSignal(tween, "finished");
+        }
+
+        public static async Task FadeOut(
+            this Control control,
+            float duration,
+            Tween.EaseType ease = Tween.EaseType.Out,
+            Tween.TransitionType trans = Tween.TransitionType.Linear
+        )
+        {
+            // Tween out
+            var tween = control
+                .GetTree()
+                .CreateTween()
+                .TweenProperty(control, "modulate:a", 0f, duration)
+                // Set ease and trans type
+                .SetEase(ease)
+                .SetTrans(trans);
+            await control.ToSignal(tween, "finished");
+
+            control.Hide();
+        }
+    }
+}
+
