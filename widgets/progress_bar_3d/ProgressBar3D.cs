aa64d594c7dab7027b8da214489a872f26c073b4
Thomsen Cummings <thomsencummings@gmail.com>
2024-01-13 12:35:42 -0500

    add in 3d progress bar widget

---
+++ widgets/progress_bar_3d/ProgressBar3D.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+using ShopIsDone.Utils.Extensions;
+
+namespace ShopIsDone.Widgets
+{
+    public partial class ProgressBar3D : Node3D
+    {
+        public float BarValue
+        {
+            get { return GetBarValue(); }
+            set { SetBarValue(value); }
+        }
+
+        private float GetBarValue()
+        {
+            return (float)_ProgressBar?.Value;
+        }
+
+        private void SetBarValue(float value)
+        {
+            if (_ProgressBar != null) _ProgressBar.Value = value;
+        }
+
+        [Export]
+        private ProgressBar _ProgressBar;
+
+        public void TweenValue(float value)
+        {
+            GetTree()
+                .CreateTween()
+                .TweenProperty(_ProgressBar, "value", value, 0.15f)
+                .SetTrans(Tween.TransitionType.Bounce)
+                .SetEase(Tween.EaseType.OutIn);
+        }
+
+        public void FadeIn(float duration)
+        {
+            Task _ = _ProgressBar.FadeIn(duration);
+        }
+
+        public void FadeOut(float duration)
+        {
+            Task _ = _ProgressBar.FadeOut(duration);
+        }
+    }
+}
+
