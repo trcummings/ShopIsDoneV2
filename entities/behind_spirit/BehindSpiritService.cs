71761e90c2eb84cb60a8eb6cc65c1ce2726eb92e
Thomsen Cummings <thomsencummings@gmail.com>
2024-01-13 15:10:26 -0500

    create null action point handler collection; add affine texture shader to behind spirit model

---
+++ entities/behind_spirit/BehindSpiritService.cs
@@ -0,0 +1,15 @@
+using Godot;
+using ShopIsDone.Core;
+using System;
+
+namespace ShopIsDone.Entities.BehindSpirit
+{
+    public partial class BehindSpiritService : Node
+    {
+        [Export]
+        private LevelEntity _BehindSpirit;
+
+
+    }
+}
+
