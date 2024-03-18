c4857aa6c92c81a9136e34ebdbb7a04ac108c8b5
Thomsen Cummings <thomsencummings@gmail.com>
2024-03-18 17:56:22 -0400

    have clown puppet info reject attempts to get more info

--- entities/clown_puppet/ClownPuppetInfoUI.cs
+++ entities/clown_puppet/ClownPuppetInfoUI.cs
@@ -1,14 +1,22 @@
 using Godot;
 using ShopIsDone.Arenas.UI;
+using ShopIsDone.Cameras;
 using ShopIsDone.Core;
+using ShopIsDone.Utils.DependencyInjection;
 using System;
 
 namespace ShopIsDone.Entities.ClownPuppet
 {
     public partial class ClownPuppetInfoUI : TargetInfoUI
     {
+        [Signal]
+        public delegate void ClownEffectEventHandler();
+
         private TextureRect _ClownFeed;
         private bool _IsAvailable = true;
+
+        [Inject]
+        private ScreenshakeService _Screenshake;
 
         public override void _Ready()
         {
@@ -28,6 +36,7 @@ namespace ShopIsDone.Entities.ClownPuppet
         public override void Init(LevelEntity entity)
         {
             base.Init(entity);
+            InjectionProvider.Inject(this);
 
             var viewport = entity.GetNode<SubViewport>("FaceCamViewport");
             _ClownFeed.Texture = viewport.GetTexture();
@@ -36,13 +45,18 @@ namespace ShopIsDone.Entities.ClownPuppet
         public override void RequestInfoPayload(Action<MoreInfoPayload> _)
         {
             // Emit more info warning effect
-            GD.Print("No no no. No.");
+            var events = Events.GetEvents(this);
+            events.EmitSignal(nameof(events.BloodWipeRequested));
+            EmitSignal(nameof(ClownEffect));
+
+            // Screenshake
+            _Screenshake.Shake(ScreenshakeHandler.ShakePayload.ShakeSizes.Huge);
 
             // Set availability flag
             _IsAvailable = false;
 
             // Set three second timer to disable UI for
-            GetTree().CreateTimer(3).Connect(
+            GetTree().CreateTimer(5).Connect(
                 "timeout",
                 Callable.From(() => _IsAvailable = true),
                 (uint)ConnectFlags.OneShot
