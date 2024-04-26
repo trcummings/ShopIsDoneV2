cef721c5115108c5967272e95678b40dc4b6b062
Thomsen Cummings <thomsencummings@gmail.com>
2024-04-26 11:36:30 -0400

    create obscene gesture microgame

--- microgames/obscene_gesture/ObsceneGestureMicrogame.cs
+++ microgames/obscene_gesture/ObsceneGestureMicrogame.cs
@@ -1,25 +1,31 @@
+using System;
+using Godot;
+
 namespace ShopIsDone.Microgames.ObsceneGesture
 {
     public partial class ObsceneGestureMicrogame : Microgame
 	{
+        [Export]
+        private AnimationPlayer _AnimPlayer;
+
         public override void Init(MicrogamePayload payload)
         {
             base.Init(payload);
             HideTimer(0.001f);
         }
+
         public override void Start()
         {
-            // Give this thing half the number of beats
-            NumBeats = 5;
-
-            // Base call
-            base.Start();
-        }
-
-        protected override void OnTimerFinished()
-        {
-            // After timer is finished, just emit an outcome
-            EmitSignal(nameof(MicrogameFinished), (int)Outcomes.Loss);
+            // Oneshot connect to the animation player
+            _AnimPlayer.Connect(
+                "animation_finished",
+                Callable.From((string _) =>
+                {
+                    EmitSignal(nameof(MicrogameFinished), (int)Outcomes.Loss);
+                }),
+                (uint)ConnectFlags.OneShot
+            );
+            _AnimPlayer.Play("default");
         }
     }
 }
